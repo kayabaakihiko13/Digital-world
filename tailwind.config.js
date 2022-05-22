25ba3cedebaef943771900397e71c8b5fc6652f4
kayabaakhito13 <iqbalramad75@gmail.com>
2022-05-22 19:34:25 +0700

    Digital world

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center:true,
+      padding:'16px',
+
+    },
+    extend: {
+      colors:{
+        primary:'#0369a1',
+        secondary:'#64748b',
+        dark:'#0f172a',
+        green:'#34d399',
+        
+      },
+      screens:{
+        '2xl':'1420px',
+      }
+    },
+  },
+  plugins: [],
+}
